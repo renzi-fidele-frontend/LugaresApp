6fbdfd548a087361a5c953cfbe1ec9ac450955df
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-04-27 22:32:29 +0200

    Adicionando a estrutura básica de página adicionar lugar

--- src/pages/AdicionarLugar/AdicionarLugar.jsx
+++ src/pages/AdicionarLugar/AdicionarLugar.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./AdicionarLugar.module.css";
+import { Col, Container, Form, Image, Row } from "react-bootstrap";
+import foto from "../../assets/addLugarIco2.png";
 
 const AdicionarLugar = () => {
-   return <div id={styles.ct}>AdicionarLugar</div>;
+   return (
+      <Container className="py-5">
+         <Row className="mt-4">
+            <Col xs={7}>
+               <h2 className="mb-5">Adicione um novo lugar a plataforma</h2>
+               <Form>
+                  <Form.Group className="mb-3">
+                     <Form.Label>Nome do lugar</Form.Label>
+                     <Form.Control type="text" placeholder="Insira o nome do lugar" />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                     <Form.Label>Descrição</Form.Label>
+                     <Form.Control as="textarea" placeholder="Insira uma descrição para este lugar" />
+                  </Form.Group>
+                  <Form.Group >
+                     <Form.Label>Endereço</Form.Label>
+                     <Form.Control type="text" placeholder="Insira o endereço para este lugar" />
+                  </Form.Group>
+               </Form>
+            </Col>
+            <Col className="d-flex align-items-end">
+               <Image className="ms-auto" id={styles.fotoLado} src={foto} />
+            </Col>
+         </Row>
+      </Container>
+   );
 };
 
 export default AdicionarLugar;

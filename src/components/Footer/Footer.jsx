f0cde5b6e84169be79a2ef04b2e2c691592150af
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-04-27 18:52:29 +0200

    Responsivo do footer finalizado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,15 @@ import { Link } from "react-router-dom";
 const Footer = () => {
    return (
       <footer className="py-3 mt-4 bg-bg-dark  ">
-         <Container className="d-flex justify-content-between align-items-center pt-2 text-white border-top border-secondary ">
-            <Col className="d-flex flex-row  align-items-center  ">
+         <Container
+            fluid="lg"
+            className="d-flex flex-column flex-md-row justify-content-between align-items-center pt-2 text-white border-top border-secondary "
+         >
+            <Col
+               md={8}
+               xs={12}
+               className="d-flex flex-column flex-sm-row align-items-center justify-content-center  justify-content-md-start gy-sm-0 gy-  "
+            >
                <Link to="/" className="fw-semibold fs-4 mb-0 link-underline-opacity-0 link-underline-opacity-75-hover link-light">
                   LugaresApp
                </Link>

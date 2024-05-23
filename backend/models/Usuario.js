c7ff92b33b529879649382ba7792878a4622325a
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-05-23 23:47:58 +0200

    Criando o schema e o modelo do usuario

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -1,1 +1,14 @@
-//  TODO: Criar schema e modelo do Usuário
+const mongoose = require("mongoose");
+
+const schemaDoUsuario = new mongoose.Schema(
+   {
+      nome: { type: String, required: true },
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+   },
+   { collection: "usuarios" }
+);
+
+const Usuario = mongoose.model("Usuario", schemaDoUsuario);
+
+module.exports = Usuario;

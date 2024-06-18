0fb3c3b055f366b5d5244cbb31d6088e8d9569f6
renzi-fidele-frontend <renzifidele2001@gmail.com>
2024-06-19 01:21:53 +0200

    Instalando o cors e liberando o url do deploy para acesso ao backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -9,6 +10,8 @@ const rotalugares = require("./routes/lugares-route");
 const rotaUsuarios = require("./routes/usuarios-route");
 
 const app = express();
+
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
 console.log(process.env.PORT);
 
